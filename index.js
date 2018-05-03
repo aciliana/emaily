132dee3aef6c314432a0745013aed99f40b58ba7
aciliana <alicia.yu45@yahoo.com>
2018-05-03 12:57:08 -0400

    mongoose set up

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-//calling express like a function here creates a new seperate express app and we can have multiple express instances in an application
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+//calling express like af function here creates a new seperate express app and we can have multiple express instances in an application
+
+mongoose.connect('keys.mongoURI');
+
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); //This is express telling node to listen to port 5000
