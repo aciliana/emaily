66fdad82f26b7077c055c42b82796dfc152d52e7
aciliana <alicia.yu45@yahoo.com>
2018-05-04 23:30:05 -0400

    set up prod env

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 //calling express like af function here creates a new seperate express app and we can have multiple express instances in an application
 
-mongoose.connect('keys.mongoURI');
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 *24 *60*60*100,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
